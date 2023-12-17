3a9c03ad95d3f748cac49541d8d1321d859930c4
Idsen06 <99011151+Idsen06@users.noreply.github.com>
2023-12-17 19:15:35 +0100

    sidste clean
    clean

--- src/pages/top-50-movies.jsx
+++ src/pages/top-50-movies.jsx
@@ -34,7 +34,7 @@ const TopFiftyMovies = () => {
           throw new Error('Network response was not ok.');
         }
         const frontendJsonData = await frontendResponse.json();
-        setFrontendData(frontendJsonData); // Update state with fetched data
+        setFrontendData(frontendJsonData);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
